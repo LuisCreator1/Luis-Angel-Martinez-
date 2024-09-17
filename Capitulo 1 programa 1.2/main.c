e43a97320a614ae0e32b6094fdfadc2baf267212
LuisCreator1 <lamartinezbotello@gmail.com>
2024-09-17 14:22:27 -0400

    Add files via upload

---
+++ Capitulo 1 programa 1.2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Invierte datos
+El programa, al recibir como datos un conjunto de datos de entrada, invierte el
+orden de los mismos cuando los prime.
+
+A, B, C, D: variables de tipo entero. */
+
+void main (void)
+{
+ int A, B, C, D;
+ printf("ingrese cuatro datos de tipo entero:");
+ scanf("%d %d %d %d", &A, &B, &C, &D);
+ printf("\n &d &d &d &d ", D, C, B, A);
+}
