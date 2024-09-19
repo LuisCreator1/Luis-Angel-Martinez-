12867141fe6a37d179f290b67b6e078716f088f4
LuisCreator1 <lamartinezbotello@gmail.com>
2024-09-18 20:23:55 -0400

    Add files via upload

---
+++ Capitulo 1 programa 1.6/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Aplicacion de operadores. */
+
+void main (void)
+{
+int i = 15, j, k, 1;
+
+j = (15 > i --) > (14 < ++i);
+ptintf("\nEl valor de j es: %d" , j);
+
+k = ! ('b' != 'd') > (!i - 1);
+ptintf ("\nEl valor de k es: %d , k");
+
+1 = (! (34 > (70 % 2)) || 0);
+printf("\nEl valor de 1 es: %d" , 1);
+}
