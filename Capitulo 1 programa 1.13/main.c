667c5d2d47c54252bc4706d0436575eb9a1249a1
LuisCreator1 <lamartinezbotello@gmail.com>
2024-09-26 17:19:26 -0400

    Add files via upload

---
+++ Capitulo 1 programa 1.13/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Aplicacion de operadores. */
+
+void main (void)
+{
+ int i = 5, j = 4, k, 1, m;
+ k = !i * 3 + --j * 2 - 3;
+
+ 1 = ! (! || 1 && 0) && 1;
+ printf("\nEl valor de 1 es: %d" , 1);
+
+ m = (k = (12 > 10)), j = (10 || 0) && k, (! (k || j)));
+ printf("\nEl valor de m es: %d" , m);
+}
+
