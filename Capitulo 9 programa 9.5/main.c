ade469d83b2a083df6a154eebbe4ac8e2f6fa94d
LuisCreator1 <lamartinezbotello@gmail.com>
2024-11-28 22:13:29 -0400

    Add files via upload

---
+++ Capitulo 9 programa 9.5/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/* Archivos con variables enteras y reales.
+El programa almacena datos de un grupo de alumnos en un archivo. */
+
+void main(void)
+{
+    int i, j, n, mat;
+    float cal;
+    FILE *ar;
+
+    printf("\nIngrese el número de alumnos: ");
+    scanf("%d", &n);
+    /* Se asume que el valor que ingresa el usuario está comprendido entre 1 y 35. */
+
+    if ((ar = fopen("arc8.txt", "w")) != NULL)
+    {
+        fprintf(ar, "%d\n", n); /* Se escribe el número de alumnos en el archivo. */
+
+        for (i = 0; i < n; i++)
+        {
+            printf("\nIngrese la matrícula del alumno %d: ", i + 1);
+            scanf("%d", &mat);
+            fprintf(ar, "%d ", mat); /* Se escribe la matrícula en el archivo. */
+
+            for (j = 0; j < 5; j++)
+            {
+                printf("\nCalificación %d: ", j + 1);
+                scanf("%f", &cal);
+                fprintf(ar, "%.2f ", cal); /* Se escriben las calificaciones en el archivo. */
+            }
+            fprintf(ar, "\n"); /* Se añade un salto de línea al final de las calificaciones de cada alumno. */
+        }
+        fclose(ar);
+    }
+    else
+    {
+        printf("No se puede abrir el archivo\n");
+    }
+}
